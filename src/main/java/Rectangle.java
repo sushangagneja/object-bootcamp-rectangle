b879daeb5de7fd109dac1408525bf279fa240e50
Sushan <sushan.gagneja@thoughtworks.com>
2019-12-03 13:01:24 +0530

    Adds rectangle with length and breadth gives area

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,14 @@
+//understands a four-sided polygon with perpendicular adjacent sides
+public class Rectangle {
+    private final int length;
+    private final int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int area() {
+        return length * breadth;
+    }
+}
